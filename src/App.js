42598ba3f42001d4f55e1dda40e2687e0401ca89
gyojinK <gyojink@gyojinKui-MacBookAir.local>
2023-03-20 22:36:30 +0900

    study JSX Syntax, React state & Syntax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let post = '강남 우동 맛집';
+    let [글제목, b] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']); 
+    /* 
+    Destructuring 문법
+        let num = [1, 2]; <--여기 있던걸 변수로 빼고 싶다
+        let a = num[0]; >> 1
+        let c = num[1]; >> 2
+                ==
+        let [a, c] = [1, 2]; <-- 각각 변수에 대입시키는 문법 
+    */
+
+    return (
+        <div className="App">
+            <div className="black-nav">
+                <h4>ReactBlog</h4>
+            </div>
+            <div className='list'>
+                <h4>{ 글제목[0] }</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className='list'>
+                <h4>{ 글제목[1] }</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className='list'>
+                <h4>{ 글제목[2] }</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <h4>{post}</h4>
+        </div>
+    );
 }
 
 export default App;
